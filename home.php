82fbd24ad7caf18cc83dc00f9013ed5e2850a3ec
santiago-federici <santiagofederici88@gmail.com>
2023-10-26 13:18:54 -0300

    Update: home.php and styles

---
+++ home.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ecommerce</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="home.css">
+</head>
+
+<body>
+    <header class='page-header'>
+        <img class='logo'
+            src="https://imgs.search.brave.com/Y2amAtUn2KuQmNcdLCmoFPg0LbYsgFBghjPg97Fv5Lg/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9sb2dv/LmNvbS9pbWFnZS1j/ZG4vaW1hZ2VzL2t0/czkyOHBkL3Byb2R1/Y3Rpb24vMDA4OWI3/YWUxZWQzOTRmMDQx/YzVmNzkyOWUxMTFj/MTFlOGVhZmU0ZC00/MjR4NDIxLnBuZz93/PTEwODAmcT03Mg"
+            alt="Logo">
+
+        <nav class='header-nav'>
+                <a href="/webPage/">
+                        Home
+                </a>
+                <a href="./products.php">
+                        Products
+                </a>
+                <a href="">
+                        About
+                </a>
+        </nav>
+
+        <div class="register-btns">
+            <a href="./form/loginForm.php"><button class='secondary-btn'>Login</button></a>
+            <a href="./form/registerForm.php"><button class='primary-btn'>Register</button></a>
+        </div>
+    </header>
+
+  <main class="wrapper">
+    <section class='hero-section'>
+        <div class='hero-info'>
+            <h1>ECOMMERCE</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet perferendis animi natus fugiat aliquid quo obcaecati dolore, ut, architecto, sit maxime fuga rerum corporis ea!</p>
+        </div>
+
+        <img 
+        src="./images/computer.jpeg" 
+        alt=""
+        class='hero-img'>
+    </section>
+
+
+
+    <section class='users-container'>
+      <article class='user-card'>
+        <img src="https://randomuser.me/api/portraits/men/33.jpg" alt="" class='user-img'>
+        <div class='user-info'>
+          <h3 class='user-info__name'>Santiago Federici</h3>
+          <p class='user-info__description'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores, deleniti.</p>
+          
+          <!-- Stars -->
+          <div class='stars-container'>
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-star" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+              <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+              <path d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z"></path>
+            </svg>
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-star" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+              <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+              <path d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z"></path>
+            </svg>
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-star" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+              <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+              <path d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z"></path>
+            </svg>
+          </div>
+        </div>
+      </article>
+
+      <article class='user-card'>
+        <img src="https://randomuser.me/api/portraits/women/68.jpg" alt="" class='user-img'>
+        <div class='user-info'>
+          <h3 class='user-info__name'>Santiago Federici</h3>
+          <p class='user-info__description'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores, deleniti.</p>
+          
+          <!-- Stars -->
+          <div class='stars-container'>
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-star" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+              <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+              <path d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z"></path>
+            </svg>
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-star" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+              <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+              <path d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z"></path>
+            </svg>
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-star" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+              <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+              <path d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z"></path>
+            </svg>
+          </div>
+        </div>
+      </article>
+      
+      <article class='user-card'>
+        <img src="https://randomuser.me/api/portraits/men/93.jpg" alt="" class='user-img'>
+        <div class='user-info'>
+          <h3 class='user-info__name'>Santiago Federici</h3>
+          <p class='user-info__description'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores, deleniti.</p>
+          
+          <!-- Stars -->
+          <div class='stars-container'>
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-star" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+              <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+              <path d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z"></path>
+            </svg>
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-star" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+              <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+              <path d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z"></path>
+            </svg>
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-star" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+              <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+              <path d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z"></path>
+            </svg>
+          </div>
+        </div>
+      </article>
+
+
+    </section>
+
+    <section class='charts-container'>
+        <article class='chart-card'>
+            <img src="./images/chartOne.png" alt="">
+            <div class='chart-info'>
+                <h3>Lorem, ipsum dolor sit </h3>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse modi maxime, hic qui voluptate neque totam voluptates repellat sint rerum, non assumenda voluptatum provident natus.</p>
+            </div>
+        </article>
+        <article class='chart-card'>
+            <div class='chart-info'>
+                <h3>Lorem, ipsum dolor sit </h3>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse modi maxime, hic qui voluptate neque totam voluptates repellat sint rerum, non assumenda voluptatum provident natus.</p>
+            </div>
+            <img src="./images/chartTwo.png" alt="">
+        </article>
+    </section>
+  </main>
+
+    <footer>
+    </footer>
+</body>
+
+</html>
