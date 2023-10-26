893d2cd50ea88abc40c5e68432a8dcadd4935963
santiago-federici <santiagofederici88@gmail.com>
2023-10-26 13:24:37 -0300

    Update: products.php

---
+++ products.php
@@ -0,0 +1,228 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ecommerce | Products</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="products.css">
+</head>
+
+<body>
+    <header class='page-header'>
+        <img class='logo'
+            src="https://imgs.search.brave.com/Y2amAtUn2KuQmNcdLCmoFPg0LbYsgFBghjPg97Fv5Lg/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9sb2dv/LmNvbS9pbWFnZS1j/ZG4vaW1hZ2VzL2t0/czkyOHBkL3Byb2R1/Y3Rpb24vMDA4OWI3/YWUxZWQzOTRmMDQx/YzVmNzkyOWUxMTFj/MTFlOGVhZmU0ZC00/MjR4NDIxLnBuZz93/PTEwODAmcT03Mg"
+            alt="Logo">
+
+        <nav class='header-nav'>
+                <a href="/webPage/">
+                        Home
+                </a>
+                <a href="./products.php">
+                        Products
+                </a>
+                <a href="">
+                        About
+                </a>
+        </nav>
+
+        <div class="register-btns">
+            <a href="./form/loginForm.php"><button class='secondary-btn'>Login</button></a>
+            <a href="./form/registerForm.php"><button class='primary-btn'>Register</button></a>
+        </div>
+    </header>
+
+    <main class="wrapper">
+        <h1 class='prods-h1'>Check out our products here</h2>
+
+        <div class="prod-cards-container">
+            <article class="prod-card">
+                <img class="prod-img"
+                    src="https://deliver.com.ar/wp-content/uploads/2022/03/PORTADA-HOODIE-CLASSIC-FRISADO-22-35-scaled.jpg.webp"
+                    alt="">
+                <div class="prod-info">
+                    <h3>Product<span> - $9000</span></h3>
+                    <button class="secondary-btn">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-shopping-cart-plus"
+                            width="20" height="20" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
+                            fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                            <path d="M4 19a2 2 0 1 0 4 0a2 2 0 0 0 -4 0"></path>
+                            <path d="M12.5 17h-6.5v-14h-2"></path>
+                            <path d="M6 5l14 1l-.86 6.017m-2.64 .983h-10.5"></path>
+                            <path d="M16 19h6"></path>
+                            <path d="M19 16v6"></path>
+                        </svg>
+                    </button>
+                </div>
+            </article>
+            <article class="prod-card">
+                <img class="prod-img"
+                    src="https://deliver.com.ar/wp-content/uploads/2022/03/PORTADA-HOODIE-CLASSIC-FRISADO-22-35-scaled.jpg.webp"
+                    alt="">
+                <div class="prod-info">
+                    <h3>Product<span> - $9000</span></h3>
+                    <button class="secondary-btn">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-shopping-cart-plus"
+                            width="20" height="20" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
+                            fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                            <path d="M4 19a2 2 0 1 0 4 0a2 2 0 0 0 -4 0"></path>
+                            <path d="M12.5 17h-6.5v-14h-2"></path>
+                            <path d="M6 5l14 1l-.86 6.017m-2.64 .983h-10.5"></path>
+                            <path d="M16 19h6"></path>
+                            <path d="M19 16v6"></path>
+                        </svg>
+                    </button>
+                </div>
+            </article>
+            <article class="prod-card">
+                <img class="prod-img"
+                    src="https://deliver.com.ar/wp-content/uploads/2022/03/PORTADA-HOODIE-CLASSIC-FRISADO-22-35-scaled.jpg.webp"
+                    alt="">
+                <div class="prod-info">
+                    <h3>Product<span> - $9000</span></h3>
+                    <button class="secondary-btn">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-shopping-cart-plus"
+                            width="20" height="20" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
+                            fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                            <path d="M4 19a2 2 0 1 0 4 0a2 2 0 0 0 -4 0"></path>
+                            <path d="M12.5 17h-6.5v-14h-2"></path>
+                            <path d="M6 5l14 1l-.86 6.017m-2.64 .983h-10.5"></path>
+                            <path d="M16 19h6"></path>
+                            <path d="M19 16v6"></path>
+                        </svg>
+                    </button>
+                </div>
+            </article>
+            <article class="prod-card">
+                <img class="prod-img"
+                    src="https://deliver.com.ar/wp-content/uploads/2022/03/PORTADA-HOODIE-CLASSIC-FRISADO-22-35-scaled.jpg.webp"
+                    alt="">
+                <div class="prod-info">
+                    <h3>Product<span> - $9000</span></h3>
+                    <button class="secondary-btn">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-shopping-cart-plus"
+                            width="20" height="20" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
+                            fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                            <path d="M4 19a2 2 0 1 0 4 0a2 2 0 0 0 -4 0"></path>
+                            <path d="M12.5 17h-6.5v-14h-2"></path>
+                            <path d="M6 5l14 1l-.86 6.017m-2.64 .983h-10.5"></path>
+                            <path d="M16 19h6"></path>
+                            <path d="M19 16v6"></path>
+                        </svg>
+                    </button>
+                </div>
+            </article>
+            <article class="prod-card">
+                <img class="prod-img"
+                    src="https://deliver.com.ar/wp-content/uploads/2022/03/PORTADA-HOODIE-CLASSIC-FRISADO-22-35-scaled.jpg.webp"
+                    alt="">
+                <div class="prod-info">
+                    <h3>Product<span> - $9000</span></h3>
+                    <button class="secondary-btn">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-shopping-cart-plus"
+                            width="20" height="20" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
+                            fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                            <path d="M4 19a2 2 0 1 0 4 0a2 2 0 0 0 -4 0"></path>
+                            <path d="M12.5 17h-6.5v-14h-2"></path>
+                            <path d="M6 5l14 1l-.86 6.017m-2.64 .983h-10.5"></path>
+                            <path d="M16 19h6"></path>
+                            <path d="M19 16v6"></path>
+                        </svg>
+                    </button>
+                </div>
+            </article>
+            <article class="prod-card">
+                <img class="prod-img"
+                    src="https://deliver.com.ar/wp-content/uploads/2022/03/PORTADA-HOODIE-CLASSIC-FRISADO-22-35-scaled.jpg.webp"
+                    alt="">
+                <div class="prod-info">
+                    <h3>Product<span> - $9000</span></h3>
+                    <button class="secondary-btn">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-shopping-cart-plus"
+                            width="20" height="20" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
+                            fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                            <path d="M4 19a2 2 0 1 0 4 0a2 2 0 0 0 -4 0"></path>
+                            <path d="M12.5 17h-6.5v-14h-2"></path>
+                            <path d="M6 5l14 1l-.86 6.017m-2.64 .983h-10.5"></path>
+                            <path d="M16 19h6"></path>
+                            <path d="M19 16v6"></path>
+                        </svg>
+                    </button>
+                </div>
+            </article>
+            <article class="prod-card">
+                <img class="prod-img"
+                    src="https://deliver.com.ar/wp-content/uploads/2022/03/PORTADA-HOODIE-CLASSIC-FRISADO-22-35-scaled.jpg.webp"
+                    alt="">
+                <div class="prod-info">
+                    <h3>Product<span> - $9000</span></h3>
+                    <button class="secondary-btn">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-shopping-cart-plus"
+                            width="20" height="20" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
+                            fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                            <path d="M4 19a2 2 0 1 0 4 0a2 2 0 0 0 -4 0"></path>
+                            <path d="M12.5 17h-6.5v-14h-2"></path>
+                            <path d="M6 5l14 1l-.86 6.017m-2.64 .983h-10.5"></path>
+                            <path d="M16 19h6"></path>
+                            <path d="M19 16v6"></path>
+                        </svg>
+                    </button>
+                </div>
+            </article>
+            <article class="prod-card">
+                <img class="prod-img"
+                    src="https://deliver.com.ar/wp-content/uploads/2022/03/PORTADA-HOODIE-CLASSIC-FRISADO-22-35-scaled.jpg.webp"
+                    alt="">
+                <div class="prod-info">
+                    <h3>Product<span> - $9000</span></h3>
+                    <button class="secondary-btn">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-shopping-cart-plus"
+                            width="20" height="20" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
+                            fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                            <path d="M4 19a2 2 0 1 0 4 0a2 2 0 0 0 -4 0"></path>
+                            <path d="M12.5 17h-6.5v-14h-2"></path>
+                            <path d="M6 5l14 1l-.86 6.017m-2.64 .983h-10.5"></path>
+                            <path d="M16 19h6"></path>
+                            <path d="M19 16v6"></path>
+                        </svg>
+                    </button>
+                </div>
+            </article>
+            <article class="prod-card">
+                <img class="prod-img"
+                    src="https://deliver.com.ar/wp-content/uploads/2022/03/PORTADA-HOODIE-CLASSIC-FRISADO-22-35-scaled.jpg.webp"
+                    alt="">
+                <div class="prod-info">
+                    <h3>Product<span> - $9000</span></h3>
+                    <button class="secondary-btn">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-shopping-cart-plus"
+                            width="20" height="20" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
+                            fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                            <path d="M4 19a2 2 0 1 0 4 0a2 2 0 0 0 -4 0"></path>
+                            <path d="M12.5 17h-6.5v-14h-2"></path>
+                            <path d="M6 5l14 1l-.86 6.017m-2.64 .983h-10.5"></path>
+                            <path d="M16 19h6"></path>
+                            <path d="M19 16v6"></path>
+                        </svg>
+                    </button>
+                </div>
+            </article>
+
+        </div>
+    </main>
+
+    <footer>
+    </footer>
+</body>
+
+</html>
